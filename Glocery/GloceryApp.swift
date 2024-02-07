a64ccd481ce340e125a56bd3d42fc928b39448d1
Ezea Victor Chukwuebuka <53367180+Victorvikson1996@users.noreply.github.com>
2024-02-07 01:01:48 +0100

    Initial Commit

---
+++ Glocery/GloceryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GloceryApp.swift
+//  Glocery
+//
+//  Created by Victor Ezea on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct GloceryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
